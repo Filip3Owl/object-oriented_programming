ac78bdaa531a159d5ba798c013f29a072c4f7ee3
filipe <solarcubix@gmail.com>
2025-06-29 18:27:57 -0300

    class method

--- object-oriented programming/folder1/4.class-methods.py
+++ object-oriented programming/folder1/4.class-methods.py
@@ -2,7 +2,8 @@ class Sigma:
     def action(self):
         print("Sigma action")
 
-alfa = Sigma()
+alfa = Sigma() # Create an instance of Sigma
+type(alfa)  # <class '__main__.Sigma'>
 
 alfa.action()  # Sigma action
 
@@ -11,3 +12,15 @@ alfa.action()  # Sigma action
 class Red:
     def  __init__(self, color):
         self.color = color # This is an instance variable
+
+class Computer:
+    def __init__(self, brand, model, price): # Constructor method
+        self.brand = brand
+        self.model = model
+        self.price = price
+
+factor = Computer("Factor", "X1", 5000)
+
+print(factor.brand)  # Factor
+print(factor.model)  # X1
+print(factor.price)  # 5000
