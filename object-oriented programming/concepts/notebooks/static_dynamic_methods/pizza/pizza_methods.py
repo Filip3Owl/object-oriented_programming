ad666b10a0f6e094a5a2e0e0f7491274cb46fc76
Filipe <solarcubix@gmail.com>
2025-07-10 20:31:01 -0300

    feat: factory methods

--- object-oriented programming/concepts/notebooks/static_dynamic_methods/pizza/pizza_methods.py
+++ object-oriented programming/concepts/notebooks/static_dynamic_methods/pizza/pizza_methods.py
@@ -11,3 +11,36 @@ print(a_pizza)
 
 # So, when should you use instance methods? In short, you should use instance methods when you need to access and edit the data that an instance of your class holds.
 
+#When to use class methods
+
+#Some pizza variations
+
+Pizza(['mozzarella', 'tomatoes'])
+Pizza(['mozzarella', 'tomatoes', 'ham', 'mushrooms'])
+Pizza(['mozzarella'])
+
+# factory methods
+
+class Pizza:
+
+    def __init__(self, toppings):
+        self.toppings = list(toppings)
+
+    # __repr__ provides a string representation for when you need to display an object.
+    def __repr__(self):
+        return f"Pizza({self.toppings})"
+    
+    def add_topping(self, topping):
+        self.toppings.append(topping)
+    
+    def remove_topping(self, topping):
+        if topping in self.toppings:
+            self.toppings.remove(topping)
+    
+    @classmethod
+    def margherita(cls):
+        return cls(["mozzarella", "tomatoes"])
+    
+    @classmethod
+    def prosciutto(cls):
+        return cls(["mozzarella", "tomatoes", "ham"])
